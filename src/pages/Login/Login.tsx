9cf30b5494c23cb75cd91e1a75a27415ab6116a8
Hoàng Thành <hoangthanh5264@gmail.com>
2025-09-29 01:40:58 +0700

    merge to yen/register-form-uxui

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,33 +1,33 @@
 import React from "react";
-import { useLocation, Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 import Form from "./partials/Form/Form";
 import RightBanner from "../../components/RightBanner/RightBanner";
 
 const LogIn: React.FC = () => {
-  const { pathname: currentPath } = useLocation();
+	const { pathname: currentPath } = useLocation();
 
-  return (
-    <div className="w-full h-screen m-0 p-0 flex flex-col justify-between">
-      <div className="flex flex-row w-full flex-1">
-        <div className="flex flex-1 items-center justify-center">
-          <div className="flex flex-col items-center justify-center">
-            <Form />
-          </div>
+	return (
+		<div className="w-full h-screen m-0 p-0 flex flex-col justify-between">
+			<div className="flex flex-row w-full flex-1">
+				<div className="flex flex-1 items-center justify-center">
+					<div className="flex flex-col items-center justify-center">
+						<Form />
+					</div>
 
-          <div className="flex items-center justify-center">
-            {currentPath === "/login" && <RightBanner />}
-          </div>
-        </div>
-      </div>
-      <div className="flex justify-center items-end w-full h-[100px] pb-10">
-        <p className="text-lg font-normal p-5 text-center">
-          © 2025 AICES - AI Powered Candidate Evaluation System for Recruiters.
-          All rights Reserved
-        </p>
-      </div>
-    </div>
-  );
+					<div className="flex items-center justify-center">
+						{currentPath === "/login" && <RightBanner />}
+					</div>
+				</div>
+			</div>
+			<div className="flex justify-center items-end w-full h-[100px] pb-10">
+				<p className="text-lg font-normal p-5 text-center">
+					© 2025 AICES - AI Powered Candidate Evaluation System for Recruiters.
+					All rights Reserved
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default LogIn;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,7 @@ import { lazy } from "react";
 /* ============== Layouts ==============*/
 const MainLayout = lazy(() => import("../components/Layout/MainLayout"));
 const AdminLayout = lazy(() => import("../components/Layout/AdminLayout"));
-const Login = lazy(() => import("../pages/Login/Login"));
-// const SignUp = lazy(() => import("../pages/SignUp/SignUp"));
+
 /* ============== Candidates Pages ==============*/
 const Home = lazy(() => import("../pages/Homepage/Homepage"));
 const NotFoundPage = lazy(() => import("../pages/NotFoundPage"));
@@ -13,7 +12,10 @@ const Login = lazy(() => import("../pages/Login/Login"));
 const VerifyEmailPage = lazy(
 	() => import("../pages/CandidatePages/VerifyEmailPage")
 );
-// const SignUp = lazy(() => import("../pages/SignUp/SignUp"));
+const SignUp = lazy(() => import("../pages/SignUp/Signup"));
+const ForgetPassword = lazy(
+	() => import("../pages/Login/partials/ForgetPassword/ForgetPass")
+);
 
 /* ============== Admin Pages ==============*/
 const AdminDashboard = lazy(() => import("../pages/AdminPages/Dashboard"));
@@ -26,7 +28,9 @@ export const router = createBrowserRouter([
 		children: [{ index: true, element: <Home /> }],
 	},
 	{ path: "login", element: <Login /> },
-	// { path: "signup", element: <SignUp /> },
+	{ path: "signup", element: <SignUp /> },
+	{ path: "verify-email", element: <VerifyEmailPage /> },
+	{ path: "forgot-password", element: <ForgetPassword /> },
 
 	/* ============== Admin Pages ==============*/
 	{

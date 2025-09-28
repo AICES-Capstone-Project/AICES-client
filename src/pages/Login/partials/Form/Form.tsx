c7b571eb93a195e87b1d78d4ab9ba6231b232021
sneytaa <thy14903@gmail.com>
2025-09-28 21:23:37 +0700

    done signup and forget password uxui

--- src/pages/Login/partials/Form/Form.tsx
+++ src/pages/Login/partials/Form/Form.tsx
@@ -37,7 +37,6 @@ const Form: React.FC = () => {
 			return;
 		}
 
-		// Call API login
 		console.log("Submitting:", { email, password });
 
 		const res = await authService.login({ email, password });
@@ -55,7 +54,6 @@ const Form: React.FC = () => {
 
 	return (
 		<div className="flex flex-col gap-8">
-			{/* Heading */}
 			<div>
 				<div className="flex flex-col gap-4">
 					<div className="text-gray-900 font-inter text-[32px] font-medium">
@@ -68,7 +66,7 @@ const Form: React.FC = () => {
 						</div>
 						<div
 							className="cursor-pointer text-green-600 font-inter text-base font-medium"
-							onClick={() => navigate("/register")}
+							onClick={() => navigate("/signup")}
 						>
 							Register
 						</div>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,8 @@ import { lazy } from "react";
 const MainLayout = lazy(() => import("../components/Layout/MainLayout"));
 const AdminLayout = lazy(() => import("../components/Layout/AdminLayout"));
 const Login = lazy(() => import("../pages/Login/Login"));
-// const SignUp = lazy(() => import("../pages/SignUp/SignUp"));
+const SignUp = lazy(() => import("../pages/SignUp/Signup"));
+const ForgetPassword = lazy(() => import("../pages/Login/partials/ForgetPassword/ForgetPass"));
 /* ============== Candidates Pages ==============*/
 const Home = lazy(() => import("../pages/Homepage/Homepage"));
 const NotFoundPage = lazy(() => import("../pages/NotFoundPage"));
@@ -24,7 +25,8 @@ export const router = createBrowserRouter([
 		],
 	},
 	{ path: "login", element: <Login /> },
-	// { path: "signup", element: <SignUp /> },
+	{ path: "signup", element: <SignUp /> },
+	{ path: "forgot-password", element: <ForgetPassword /> },
 
 	/* ============== Admin Pages ==============*/
 	{
